02d2b90c4ccb3911b8b6466776347c919fba701d
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-18 14:36:40 +0200

    Osa2 tehtävä 2.1 oma komponentti kurssille, kurssin osien määrällä ei väliä

---
+++ osa2/kurssit/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
+const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
+const Sisalto = (props) => {
+  return(
+      props.kurssi.osat.map(osa=> <Osa key={osa.id} osa={osa.nimi} tehtavia={osa.tehtavia} />)
+  )
+}
+const Yhteensa = (props) => {
+  const [osa1, osa2, osa3] = props.kurssi.osat
+  
+  return(
+    <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
+  )
+}
+
+const Kurssi = (props) => {
+    return (
+    <div>
+        <Otsikko kurssi = {props.kurssi} />
+        <Sisalto kurssi = {props.kurssi} />
+    </div>
+    )   
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10,
+        id: 0
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7,
+        id: 1
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14,
+        id: 2
+      }
+    ]
+  }
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
