31b284ebf6bfa8d18ea548e9371d6f9a9121ae30
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-23 16:26:31 +0200

    Osa 2 tehtävä 2.9 Rajaa näytettävien nimien listaa

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -2,16 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Person = ({ person }) => {
-  return <li>{person.name} {person.number}</li>;
+  return (
+    <li>
+      {person.name} {person.number}
+    </li>
+  );
 };
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      persons: [{ name: "Arto Hellas", number:"040-123456" }],
+      persons: [
+        { name: "Arto Hellas", number: "040-123456" },
+        { name: "Martti Tienari", number: "040-123456" },
+        { name: "Arto Järvinen", number: "040-123456" },
+        { name: "Lea Kutvonen", number: "040-123456" }
+      ],
       newName: "",
-      newNumber: ""
+      newNumber: "",
+      filter: ""
     };
   }
 
@@ -23,7 +33,7 @@ class App extends React.Component {
       number: this.state.newNumber
     };
 
-    const names = this.state.persons.map(person => person.name)
+    const names = this.state.persons.map(person => person.name);
 
     if (!names.includes(personObject.name)) {
       const persons = this.state.persons.concat(personObject);
@@ -33,7 +43,9 @@ class App extends React.Component {
         newName: "",
         newNumber: ""
       });
-    } else {alert("Nimi on jo luettelossa!")}
+    } else {
+      alert("Nimi on jo luettelossa!");
+    }
   };
 
   handleNewPerson = event => {
@@ -44,18 +56,38 @@ class App extends React.Component {
     this.setState({ newNumber: event.target.value });
   };
 
+  handleFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
   render() {
+    let persons = this.state.persons;
+
+    if (this.state.filter) {
+      persons = this.state.persons.filter(person =>
+        person.name.toLowerCase().match(this.state.filter.toLowerCase())
+      );
+    }
+
     return (
       <div>
         <h2>Puhelinluettelo</h2>
+        <div>
+          Rajaa näytettäviä{" "}
+          <input value={this.state.filter} onChange={this.handleFilter} />
+        </div>
+        <h3>Lisää uusi</h3>
         <form onSubmit={this.addPerson}>
           <div>
             nimi:{" "}
             <input value={this.state.newName} onChange={this.handleNewPerson} />
           </div>
           <div>
-            numero: 
-            <input value={this.state.newNumber} onChange={this.handleNewNumber} />
+            numero:
+            <input
+              value={this.state.newNumber}
+              onChange={this.handleNewNumber}
+            />
           </div>
           <div>
             <button type="submit">lisää</button>
@@ -63,9 +95,7 @@ class App extends React.Component {
         </form>
         <h2>Numerot</h2>
         <ul>
-          {this.state.persons.map(person => (
-            <Person key={person.name} person={person} />
-          ))}
+          {persons.map(person => <Person key={person.name} person={person} />)}
         </ul>
       </div>
     );
