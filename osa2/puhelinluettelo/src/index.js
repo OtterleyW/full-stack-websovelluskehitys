166fa11f37747960805346433a6cc0768f864fc1
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-24 16:30:35 +0200

    Osa 2 tehtävä 2.18 Näytä ilmoitus, kun yhteystiedon lisäys, muokkaaminen tai poisto onnistuu

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import personService from "./services/persons";
+import "./index.css";
 
 const Numbers = props => {
   return (
@@ -46,6 +47,13 @@ const Form = props => {
   );
 };
 
+const Notification = props => {
+  if (!props.actionMade || props.error) {
+    return null;
+  }
+  return <div className="message">{props.message}</div>;
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -53,7 +61,10 @@ class App extends React.Component {
       persons: [],
       newName: "",
       newNumber: "",
-      filter: ""
+      filter: "",
+      error: false,
+      actionMade: false,
+      message: ""
     };
   }
 
@@ -74,13 +85,28 @@ class App extends React.Component {
     const names = this.state.persons.map(person => person.name);
 
     if (!names.includes(personObject.name)) {
-      personService.create(personObject).then(response => {
-        this.setState({
-          persons: this.state.persons.concat(response),
-          newName: "",
-          newNumber: ""
+      personService
+        .create(personObject)
+        .then(response => {
+          this.setState({
+            persons: this.state.persons.concat(response),
+            newName: "",
+            newNumber: "",
+            actionMade: true,
+            message: "Uuden henkilön lissäminen onnistui"
+          });
+          setTimeout(() => {
+            this.setState({ actionMade: false });
+          }, 5000);
+        })
+        .catch(error => {
+          this.setState({
+            error: true
+          });
+          setTimeout(() => {
+            this.setState({ actionMade: false, error: false });
+          }, 5000);
         });
-      });
     } else {
       if (
         window.confirm(
@@ -94,15 +120,30 @@ class App extends React.Component {
         );
         const newPerson = { ...person, number: this.state.newNumber };
 
-        personService.update(person.id, newPerson).then(response => {
-          this.setState({
-            persons: this.state.persons.map(
-              person => (person.id !== newPerson.id ? person : newPerson)
-            ),
-            newName: "",
-            newNumber: ""
+        personService
+          .update(person.id, newPerson)
+          .then(response => {
+            this.setState({
+              persons: this.state.persons.map(
+                person => (person.id !== newPerson.id ? person : newPerson)
+              ),
+              newName: "",
+              newNumber: "",
+              actionMade: true,
+              message: "Numeron muuttaminen onnistui"
+            });
+            setTimeout(() => {
+              this.setState({ actionMade: false });
+            }, 5000);
+          })
+          .catch(error => {
+            this.setState({
+              error: true
+            });
+            setTimeout(() => {
+              this.setState({ actionMade: false, error: false });
+            }, 5000);
           });
-        });
       }
     }
   };
@@ -123,8 +164,21 @@ class App extends React.Component {
     if (window.confirm("Haluatko poistaa henkilön?")) {
       personService.delet(id).then(response => {
         this.setState({
-          persons: this.state.persons.filter(person => person.id !== id)
+          persons: this.state.persons.filter(person => person.id !== id),
+          actionMade: true,
+          message: "Henkilön poistaminen onnistui"
         });
+        setTimeout(() => {
+          this.setState({ actionMade: false });
+        }, 5000);
+      })
+      .catch(error => {
+        this.setState({
+          error: true
+        });
+        setTimeout(() => {
+          this.setState({ actionMade: false, error: false });
+        }, 5000);
       });
     }
   };
@@ -141,6 +195,11 @@ class App extends React.Component {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
+        <Notification
+          message={this.state.message}
+          actionMade={this.state.actionMade}
+          error={this.state.error}
+        />
         <Filter value={this.state.filter} onChange={this.handleFilter} />
         <h3>Lisää uusi numero</h3>
         <Form
