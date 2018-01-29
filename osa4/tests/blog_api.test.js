41707d6a1a985cd5837071358c030d623240f96d
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-29 13:24:07 +0200

    Osa4 tehtävä 4.10 Tee testi, joka varmistaa, että jos kentälle likes ei anneta arvoa, asetetaan arvoksi 0

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -86,47 +86,63 @@ test("a specific blog is within the returned blogs", async () => {
   expect(titles).toContain("React patterns");
 });
 
-test('new blog can be added ', async () => {
+test("new blog can be added ", async () => {
   const newBlog = {
-    title: 'Awesome blog is awesome',
+    title: "Awesome blog is awesome",
     author: "dog",
     url: "http://www.blogi.fi"
-  }
+  };
 
   await api
-    .post('/api/blogs')
+    .post("/api/blogs")
     .send(newBlog)
     .expect(200)
-    .expect('Content-Type', /application\/json/)
+    .expect("Content-Type", /application\/json/);
 
-  const response = await api
-    .get('/api/blogs')
+  const response = await api.get("/api/blogs");
 
-  const titles = response.body.map(r => r.title)
+  const titles = response.body.map(r => r.title);
 
-  expect(response.body.length).toBe(blogs.length + 1)
-  expect(titles).toContain('Awesome blog is awesome')
-})
+  expect(response.body.length).toBe(blogs.length + 1);
+  expect(titles).toContain("Awesome blog is awesome");
+});
 
-test('blog without title is not added ', async () => {
+test("if likes is not given likes is 0", async () => {
+  const newBlog = {
+    title: "Not liked",
+    author: "dog",
+    url: "http://www.no.fi"
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  const blog = response.body.filter(r => r.title === "Not liked");
+  expect(blog[0].likes).toBe(0);
+});
+
+test("blog without title is not added ", async () => {
   const newBlog = {
     author: "Aku Ankka",
     url: "http://www.jee.fi"
-  }
+  };
 
-  const blogs = await api
-    .get('/api/blogs')
+  const blogs = await api.get("/api/blogs");
 
   await api
-    .post('/api/blogs')
+    .post("/api/blogs")
     .send(newBlog)
-    .expect(400)
+    .expect(400);
 
-  const response = await api
-    .get('/api/blogs')
+  const response = await api.get("/api/blogs");
 
-  expect(response.body.length).toBe(blogs.body.length)
-})
+  expect(response.body.length).toBe(blogs.body.length);
+});
 
 afterAll(() => {
   server.close();
