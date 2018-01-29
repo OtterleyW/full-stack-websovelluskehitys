e0a8298ce7a0860526975ea0601ee5c05f7d075f
Jenni Nurmi <jenniknurmi@gmail.com>
2018-01-29 09:10:37 +0200

    Osa 4 tehtävä 4.2 jaa sovelluksen koodi moduuleihin

--- osa4/index.js
+++ osa4/index.js
@@ -1,45 +1,27 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require("http");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const middleware = require("./utils/middleware");
+const blogsRouter = require("./controllers/blogs");
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const mongoUrl =
+  "mongodb://otter:hurraamongo@ds119028.mlab.com:19028/blogilista";
+mongoose.connect(mongoUrl);
+mongoose.Promise = global.Promise;
 
-module.exports = Blog
+app.use(cors());
+app.use(bodyParser.json());
+app.use(middleware.logger);
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use("/api/blogs", blogsRouter);
 
-const mongoUrl = 'mongodb://otter:hurraamongo@ds119028.mlab.com:19028/blogilista'
-mongoose.connect(mongoUrl)
-mongoose.Promise = global.Promise
+app.use(middleware.error);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
