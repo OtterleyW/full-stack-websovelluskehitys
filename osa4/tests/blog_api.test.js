c58b142a5d74590c64d529dc3ea657b529acf875
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-19 10:08:23 +0200

    Osa 4 tehtävä 4.12 Refaktoroi testejä

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -2,65 +2,12 @@ const supertest = require("supertest");
 const { app, server } = require("../index");
 const api = supertest(app);
 const Blog = require("../models/blog");
+const helper = require('./test_helper')
 
-const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url:
-      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url:
-      "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url:
-      "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }
-];
 
 beforeAll(async () => {
   await Blog.remove({});
-
-  const blogObjects = blogs.map(blog => new Blog(blog));
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
   const promiseArray = blogObjects.map(blog => blog.save());
   await Promise.all(promiseArray);
 });
@@ -73,9 +20,10 @@ test("blogs are returned as json", async () => {
 });
 
 test("all blogs are returned", async () => {
+  const blogsAtBeginningOfOperation = await helper.blogsInDb()
   const response = await api.get("/api/blogs");
 
-  expect(response.body.length).toBe(blogs.length);
+  expect(response.body.length).toBe(blogsAtBeginningOfOperation.length);
 });
 
 test("a specific blog is within the returned blogs", async () => {
@@ -87,6 +35,8 @@ test("a specific blog is within the returned blogs", async () => {
 });
 
 test("new blog can be added ", async () => {
+  const blogsAtBeginningOfOperation = await helper.blogsInDb()
+
   const newBlog = {
     title: "Awesome blog is awesome",
     author: "dog",
@@ -102,8 +52,9 @@ test("new blog can be added ", async () => {
   const response = await api.get("/api/blogs");
 
   const titles = response.body.map(r => r.title);
+  const blogsAfterOperation = await helper.blogsInDb()
 
-  expect(response.body.length).toBe(blogs.length + 1);
+  expect(blogsAfterOperation.length).toBe(blogsAtBeginningOfOperation.length + 1);
   expect(titles).toContain("Awesome blog is awesome");
 });
 
