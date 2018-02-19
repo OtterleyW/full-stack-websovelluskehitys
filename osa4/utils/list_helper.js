5515028880e13970d2762ff052da5f604a364a64
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-19 09:27:54 +0200

    Osa 4 tehtävä 4.6 Luo testi sille, että haetaan kirjoittaja jolla on eniten blogeja

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,34 +1,60 @@
 const dummy = blogs => {
-  return 1;
-};
+  return 1
+}
 
 const totalLikes = blogs => {
-  const likes = blogs.map(blog => blog.likes);
+  const likes = blogs.map(blog => blog.likes)
 
   const reducer = (sum, item) => {
-    return sum + item;
-  };
+    return sum + item
+  }
 
-  return likes.reduce(reducer, 0);
-};
+  return likes.reduce(reducer, 0)
+}
 
 const favoriteBlog = blogs => {
-  if(blogs.length === 0) {
-    return "No blogs"
+  if (blogs.length === 0) {
+    return 'No blogs'
   }
-  
-  let mostLikes = blogs.map(blog => blog.likes);
-  let result = mostLikes.indexOf(Math.max(...mostLikes));
+
+  let mostLikes = blogs.map(blog => blog.likes)
+  let result = mostLikes.indexOf(Math.max(...mostLikes))
 
   return {
-    "title": blogs[result].title,
-    "author": blogs[result].author,
-    "likes": blogs[result].likes
-  };
-};
+    title: blogs[result].title,
+    author: blogs[result].author,
+    likes: blogs[result].likes
+  }
+}
+
+const mostBlogs = blogs => {
+  if(blogs.length === 0){
+    return undefined
+  }
+  const authors = blogs.map(blog => blog.author)
+
+  let mostBlogs = []
+
+  authors.forEach(author => {
+    let findAuthor = mostBlogs.filter(blog => blog.author === author)
+    if (findAuthor.length !== 0) {
+      const i = mostBlogs.findIndex(obj => obj === findAuthor[0])
+      mostBlogs[i].blogs = mostBlogs[i].blogs + 1
+    } else {
+      const obj = { author: author, blogs: 1 }
+      mostBlogs.push(obj)
+    }
+  })
+
+  const writer = mostBlogs.reduce(function(prev, current) {
+    return prev.blogs > current.blogs ? prev : current
+  })
+  return writer
+}
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
-};
+  favoriteBlog,
+  mostBlogs
+}
