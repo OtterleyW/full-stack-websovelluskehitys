09c605f22027638112e43cbea540cdec508019bc
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-23 22:05:51 +0200

    Osa 5 tehtävä 5.6 tee blogin nimestä klikatta niin että näytetään enemmän tietoa

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,45 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
 
-export default Blog
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    };
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible });
+  };
+
+  render() {
+    const hideWhenVisible = { display: this.state.visible ? 'none' : '' };
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' };
+
+    const blog = this.props.blog;
+
+    const blogStyle = {
+      padding: 10,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+
+    return (
+      <div style={blogStyle}>
+        <div style={hideWhenVisible}>
+          <span onClick={this.toggleVisibility}>{blog.title} (by {blog.author})</span><br />
+        </div>
+        <div style={showWhenVisible}>
+          <span onClick={this.toggleVisibility}>{blog.title} (by {blog.author})</span><br />
+          <a href={blog.url}>{blog.url}</a><br />
+          {blog.likes} likes <button>like</button><br />
+          Added by {blog.user.username}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Blog;
