55fcded528f3333c683c019f0d7e503a4a79c0bf
Jenni Nurmi <jenniknurmi@gmail.com>
2018-02-23 21:28:58 +0200

    Osa 5 tehtävä 5.5 tee uuden blogin lisäämisestä vain tarvittaessa näytettävä togglable komponentin avulla

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from './services/login';
 import NewBlogForm from './components/NewBlogForm';
+import Togglable from './components/Toggleable';
 import './App.css';
 
 class App extends React.Component {
@@ -124,12 +125,13 @@ class App extends React.Component {
           <button onClick={this.logOut}>log out</button>
         </p>
         {this.state.notification && notification()}
-
+        <Togglable buttonLabel="new blog">
         <NewBlogForm
           setNotification={this.setNotification}
           setError={this.setError}
           getBlogs={this.getBlogs}
         />
+        </Togglable>
         <h2>blogs</h2>
         {this.state.blogs.map(blog => <Blog key={blog._id} blog={blog} />)}
       </div>
