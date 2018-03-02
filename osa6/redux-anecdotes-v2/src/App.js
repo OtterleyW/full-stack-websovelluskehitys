3a22432e6566a14ebfbcba613b579d7ee43db81c
Jenni Nurmi <jenniknurmi@gmail.com>
2018-03-02 21:35:56 +0200

    Osa 6 tehtävät 6.10 ja 6.11 hae anekdootit backendista ja lisää uusi anekdootti backendiin

--- osa6/redux-anecdotes-v2/src/App.js
+++ osa6/redux-anecdotes-v2/src/App.js
@@ -3,19 +3,32 @@ import Notification from './components/Notification';
 import Filter from './components/Filter';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
+import { anecdoteInitialization } from './reducers/anecdoteReducer';
+import anecdoteService from './services/anecdotes';
+import { connect } from 'react-redux'
+
 
 class App extends React.Component {
+  componentDidMount = async () => {
+    const anecdotes = await anecdoteService.getAll()
+    this.props.anecdoteInitialization(anecdotes)
+  }
+
   render() {
     return (
       <div>
-        <h1>Programming anecdotes</h1>
-        <Notification />
-        <Filter />
-        <AnecdoteList />
-        <AnecdoteForm />
-      </div>
-    );
+      <h1>Programming anecdotes</h1>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+    )
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { anecdoteInitialization }
+)(App)
+
--- osa6/redux-anecdotes-v2/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteForm.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { createAnecdote } from '../reducers/anecdoteReducer';
 import { notificationChange } from '../reducers/notificationReducer';
 import { connect } from 'react-redux';
+import anecdoteService from '../services/anecdotes';
 
 class AnecdoteForm extends React.Component {
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
     const content = e.target.anecdote.value;
-    this.props.createAnecdote(content);
+    e.target.anecdote.value = '';
+    const newAnecdote = await anecdoteService.createNew(content);
 
-    this.props.notificationChange(`You created anecdote: ${content}`);
+    this.props.createAnecdote(newAnecdote);
+
+    this.props.notificationChange(
+      `You created anecdote: ${newAnecdote.content}`
+    );
 
     setTimeout(() => this.props.notificationChange(null), 5000);
-
-    e.target.anecdote.value = '';
   };
   render() {
     return (
@@ -30,9 +34,9 @@ class AnecdoteForm extends React.Component {
   }
 }
 
-const ConnectedAnecdoteForm = connect(
-  null,
-  { createAnecdote, notificationChange  }
-)(AnecdoteForm);
+const ConnectedAnecdoteForm = connect(null, {
+  createAnecdote,
+  notificationChange
+})(AnecdoteForm);
 
 export default ConnectedAnecdoteForm;
